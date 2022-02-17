fbea58470dc5d6ff1a3d1af9da26354b707f8fa9
ostryJR <98188315+ostryJR@users.noreply.github.com>
2022-02-17 21:12:55 +0100

    Update consts.py

--- consts.py
+++ consts.py
@@ -5,6 +5,20 @@ RED = (255, 0, 0)
 WHITERED = (255, 80, 80)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+BROWN = (139, 69, 19)
+
+
+#to use it, write before text "consts.bcolors.OKGREEN" for green color, and to end using this color write "consts.bcolors.ENDC", all with print data
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 #MATHS CONSTS VARIBLES
 pi = 3.1415926535897932384626433832795
